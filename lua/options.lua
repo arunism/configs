68236c73ecc342bb4bd71dcb4a29dcb260fb51df
arunism <thearunism@gmail.com>
2024-06-13 22:32:38 +0545

    UPDATE: Options for Vim

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,5 @@
+vim.g.terminal_emulator = "konsole-Arun"
+
 local opt = vim.opt
 
 opt.relativenumber = true
@@ -8,5 +10,12 @@ opt.tabstop = 2
 opt.softtabstop = 2
 opt.swapfile = false
 opt.grepprg = "rg --vimgrep"
+opt.syntax = "on"
+opt.autoindent = true
+opt.cursorline = true
+opt.encoding = "utf-8"
+opt.hidden = true
+opt.showcmd = true
+opt.showmatch = true
 
 vim.g.mapleader = " "
