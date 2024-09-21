3bce774cf4091950054649be9ee49b3877fa4347
arunism <thearunism@gmail.com>
2024-09-21 12:03:25 +0545

    ADD: Configuration for Snippets and AutoCompletion

--- nvim/lua/configs/completion/cmp.lua
+++
@@ -1,8 +0,0 @@
-return function()
-  -- Define the configuration directly
-  local configs = {
-  }
-
-  -- Integrate all the configurations
-  require("project_nvim").setup(configs)
-end
--- nvim/lua/configs/language/lsp.lua
+++ nvim/lua/configs/language/lsp.lua
@@ -1,17 +1,17 @@
 return function()
+  local lspconfig = require("lspconfig")
   local lang_servers = require("core.settings").lang_servers
 
   -- Define the configuration directly
   require("configs.language.mason")()
 
   -- Integrate all the configurations
-  lspconfig = require("lspconfig")
-  -- capabilities = require("cmp_nvim_lsp").default_capabilities(
-  --   vim.lsp.protocol.make_client_capabilities()
-  -- )
+  local capabilities = require("cmp_nvim_lsp").default_capabilities(
+    vim.lsp.protocol.make_client_capabilities()
+  )
 
   for _, ls in ipairs(lang_servers) do
-    lspconfig[ls].setup({})
+    lspconfig[ls].setup({ capabilities = capabilities })
   end
 
 end
