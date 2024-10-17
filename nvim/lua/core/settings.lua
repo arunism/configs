f4081cd56e9fe4026d7838c524ca6b9e36e47d11
arunism <thearunism@gmail.com>
2024-10-17 22:50:40 +0545

    UPDATE: LSP Servers

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -3,7 +3,17 @@ local settings = {}
 
 settings["background"] = "dark"
 settings["colorscheme"] = "catppuccin"
-settings["lang_servers"] = { "lua_ls", "pylsp", "clangd", "hydra_lsp", "texlab" }
+settings["lang_servers"] = {
+  "lua_ls",
+  "pyright",
+  "clangd",
+  -- "biome",
+  "marksman",
+  "yamlls",
+  "dockerls",
+  "jsonls",
+  "harper_ls"
+}
 settings["transparent_background"] = true
 settings["use_ssh"] = true
 
