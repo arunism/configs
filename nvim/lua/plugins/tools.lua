176cfe818f544b532d8e39fd7ec894bc4c713a43
arunism <thearunism@gmail.com>
2025-03-16 19:34:40 +0545

    ADD: Remote nvim

--- nvim/lua/plugins/tools.lua
+++ nvim/lua/plugins/tools.lua
@@ -49,5 +49,15 @@ tools["Bekaboo/dropbar.nvim"] = {
 	},
 }
 
+--- REMOTE-NVIM
+tools["amitds1997/remote-nvim.nvim"] = {
+   version = "*", -- Pin to GitHub releases
+   dependencies = {
+       "nvim-lua/plenary.nvim", -- For standard functions
+       "MunifTanjim/nui.nvim", -- To build the plugin UI
+       "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+   },
+   config = require("configs.tools.remote"),
+}
 
 return tools
