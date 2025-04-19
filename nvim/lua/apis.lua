97b62858bf3d1bc3775cd4590b4b4d5c7f50f4f0
arunism <thearunism@gmail.com>
2025-04-19 21:21:01 +0545

    ADD: Yank Highligher configuration

---
+++ nvim/lua/apis.lua
@@ -0,0 +1,10 @@
+local api = vim.api
+
+
+api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -5,6 +5,7 @@ require("core.global")
 require("core.options")
 require("core.pmanager")
 require("keymaps")
+require("apis")
 
 
 vim.cmd.colorscheme(settings.colorscheme)
