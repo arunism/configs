678fce941f950f2ed4e6a2156d157da4fe094725
arunism <thearunism@gmail.com>
2025-12-06 15:06:21 +0545

    ADD: Hyprland Packages

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Add the user to the 'input' group
+sudo usermod -aG input "$(whoami)"
+
 # Get the current script directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/scripts"
 
 # Source the all scripts located in the same directory
-source "$SCRIPT_DIR/sddm.sh"
+source "$SCRIPT_DIR/packages.sh"
 source "$SCRIPT_DIR/audio.sh"
 source "$SCRIPT_DIR/bluetooth.sh"
+source "$SCRIPT_DIR/sddm.sh"
 
---
+++ scripts/packages.sh
@@ -0,0 +1,47 @@
+# List of essential Hyprland packages to install
+package_list=(
+  bc
+  curl
+  gvfs
+  gvfs-mtp
+  hyprpolkitagent
+  imagemagick
+  kitty
+  network-manager-applet
+  pamixer
+  pavucontrol
+  playerctl
+  qt5ct
+  qt6ct
+  qt6-svg
+  rofi
+  unzip
+  waybar
+  wget
+  ripgrep
+  wl-clipboard
+  wlogout
+  xdg-user-dirs
+  xdg-utils
+  brightnessctl
+  btop
+  gnome-system-monitor
+  nvtop
+  nwg-look
+  nwg-displays
+  pacman-contrib
+)
+
+# Source global functions from an external script
+source "$(dirname "$(readlink -f "$0")")/scripts/global.sh"
+
+echo -e "\n${NOTE} Installing necessary Hyprland packages..."
+for PKG in "${package_list[@]}"; do
+  install_package "$PKG"
+done
+
+echo -e "\n${OK} Installation complete."
+
+# Print two blank lines for readability
+printf "\n%.0s" {1..2}
+
